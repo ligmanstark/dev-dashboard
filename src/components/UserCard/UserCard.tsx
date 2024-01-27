88b8c5d4c806c1ece42ba3fe8b77b66dd4c596f4
levis <miduwaystark@gmail.com>
2024-01-27 20:27:42 +0400

    fix

--- src/components/UserCard/UserCard.tsx
+++ src/components/UserCard/UserCard.tsx
@@ -8,9 +8,12 @@ import { UserInfo } from '../UserInfo/UserInfo';
 import { useSelector } from 'react-redux';
 import { RootState } from '../../store/store';
 import { FC } from 'react';
+import { useRouter } from 'next/navigation';
+import { Button } from '../Button/Button';
 interface UserCardProps extends GithubUser {}
 
 export const UserCard: FC<UserCardProps> = (props: UserCardProps) => {
+  const router = useRouter()
   const isDark = useSelector((state: RootState) => state.themeReducer.isDark);
   return (
     <S.Wrapper style={!isDark ? { color: '#697c9a' } : { color: '#fff' }}>
@@ -34,6 +37,10 @@ export const UserCard: FC<UserCardProps> = (props: UserCardProps) => {
           twitter_username={props.twitter_username}
         />
       </S.UserCard>
+      <div style={{marginLeft:'19rem'}}>
+      <Button onClick={()=>router.push('http://localhost:3000/')}>Сome back</Button>
+
+      </div>
     </S.Wrapper>
   );
 };
