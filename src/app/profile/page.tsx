0554d41ffd1bf2f3937bad735e0b5acfd7bbc159
levis <miduwaystark@gmail.com>
2024-01-27 11:32:07 +0400

    initial Profile page

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,23 @@
+'use client'
+import * as S from './style'
+import { GlobalStyles } from '../../styled/GlobalStyles';
+import { Header } from '../../Layout/Header/Header';
+import { Footer } from '../../Layout/Footer/Footer';
+import { Container } from '../../styled/components';
+const Profile = () => {
+    return(
+        <>
+        <main>
+          <GlobalStyles />
+          <>
+            <Header />
+            <Container>
+                ssss
+             </Container>
+           </>
+        </main>
+      </>
+        )
+}
+
+export default Profile;
